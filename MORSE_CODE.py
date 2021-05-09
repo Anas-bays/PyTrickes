56b1d9c6d38230d86f91dec57ffe5a8ed57eec38
Anas-bays <83026708+Anas-bays@users.noreply.github.com>
2021-05-09 04:17:38 +0000

    Morse Code

---
+++ MORSE_CODE.py
@@ -0,0 +1,130 @@
+# Python program to implement Morse Code Translator
+
+'''
+VARIABLE KEY
+'cipher' -> 'stores the morse translated form of the english string'
+'decipher' -> 'stores the english translated form of the morse string'
+'citext' -> 'stores morse code of a single character'
+'i' -> 'keeps count of the spaces between morse characters'
+'message' -> 'stores the string to be encoded or decoded'
+'''
+  
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+#######  try: .- -. .- ..   ########
+  
+# Function to encrypt the string
+# according to the morse code chart
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+  
+            # Looks up the dictionary and adds the
+            # correspponding morse code
+            # along with a space to separate
+            # morse codes for different characters
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            # 1 space indicates different characters
+            # and 2 indicates different words
+            cipher += ' '
+  
+    return cipher
+  
+# Function to decrypt the string
+# from morse to english
+def decrypt(message):
+  
+    # extra space added at the end to access the
+    # last morse code
+    message += ' '
+  
+    decipher = ''
+    citext = ''
+    for letter in message:
+  
+        # checks for space
+        if (letter != ' '):
+  
+            # counter to keep track of space
+            i = 0
+  
+            # storing morse code of a single character
+            citext += letter
+  
+        # in case of space
+        else:
+            # if i = 1 that indicates a new character
+            i += 1
+  
+            # if i = 2 that indicates a new word
+            if i == 2 :
+  
+                 # adding space to separate words
+                decipher += ' '
+            else:
+  
+                # accessing the keys using their values (reverse of encryption)
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+                .values()).index(citext)]
+                citext = ''
+  
+    return decipher
+
+# Ask for repiting
+i = 0
+def question():
+    global i
+    while True:
+        print("Give another try?")
+        answer = input("(Y/N): ").upper()
+        if answer == 'Y':
+            main()
+        elif answer == 'N':
+            print("Thank you for participating")
+            break
+        else:
+            if (i < 2):
+                i += 1
+                question()
+            else:
+                print("don't fuck with us!!")
+            break
+
+
+# Hard-coded driver function to run the program
+def main():
+    print("You wanna code or decode: (C/D)")
+    give = input("Choice: ").upper()
+    if give == "C":
+        message = input("enter your message: ")
+        result = encrypt(message.upper())
+        print (f"Your Morse Code:'{result}'")
+        question()
+    elif give == 'D':
+        message = input("enter your message: ")
+        result = decrypt(message)
+        print ("The Message Is: ", result)
+        question()
+    else:
+        print("Be Serious Maan!!\n")
+        main()  
+  
+# Executes the main function
+if __name__ == '__main__':
+    main()
